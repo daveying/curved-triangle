ce52d7792b4d9016554ad56920a8affb50f759c4
daveying <daveying@163.com>
2017-08-08 21:59:26 +0800

    seperate js code out of html

---
+++ js/gui-control.js
@@ -0,0 +1,53 @@
+var iconBind = (function () {
+    var $pencilIcon = $('#data-input-icon');
+    console.log($pencilIcon);
+    var $cogIcon = $('#info-show-icon');
+    var $dataInput = $('#data-input');
+    var $infoShow = $('#info-show');
+    $dataInput.hide();
+    $infoShow.hide();
+    var dataInputOpen = false;
+    var infoShowOpen = false;
+    $pencilIcon.click(function () {
+        if (infoShowOpen) {
+            $infoShow.hide();
+            infoShowOpen = false;
+        }
+        if (dataInputOpen) {
+            $dataInput.fadeOut(1);
+            dataInputOpen = false;
+        } else {
+            $dataInput.fadeIn(1);
+            dataInputOpen = true;
+        }
+    });
+    $cogIcon.click(function () {
+        if (dataInputOpen) {
+            $dataInput.hide();
+            dataInputOpen = false;
+        }
+        if (infoShowOpen) {
+            $infoShow.fadeOut(1);
+            infoShowOpen = false;
+        } else {
+            $infoShow.fadeIn(1);
+            infoShowOpen = true;
+        }
+    });
+})();
+
+var logoCtrl = (function () {
+    // logo change
+    var logoA = document.getElementById('my-logo');
+    var logoImgGray = document.getElementById('my-logo-img-gray');
+    var logoImgWhite = document.getElementById('my-logo-img-white');
+    var originalStyle = logoImgGray.style['display'];
+    logoA.onmouseover = function (event) {
+        logoImgGray.style['display'] = "none";
+        logoImgWhite.style['display'] = originalStyle;
+    }
+    logoA.onmouseout = function (event) {
+        logoImgWhite.style['display'] = "none";
+        logoImgGray.style['display'] = originalStyle;
+    }
+})();
