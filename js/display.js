96edab377e80a7a49a7db333969ca131562eab0c
daveying <daveying@163.com>
2017-08-14 22:10:06 +0800

    add a simple example to show triangle

--- js/display.js
+++ js/display.js
@@ -13,10 +13,21 @@ var displayModule = (function () {
     camera.position.set(2, 0, 15);
     scene.add(camera);
 
-    var cube = new THREE.Mesh(new THREE.CubeGeometry(1, 2, 3),
-                            new THREE.MeshPhongMaterial({
-                                color: 0xff0000
-                            }));
+    var cube = new THREE.Mesh(new THREE.CubeGeometry(1, 2, 3), new THREE.MeshNormalMaterial());
+    var geom = new THREE.Geometry();
+    var v1 = new THREE.Vector3(0, 0, 0);
+    var v2 = new THREE.Vector3(3, 0, 0);
+    var v3 = new THREE.Vector3(3, 3, 0);
+    console.log(geom.vertices);
+    geom.vertices.push(v1);
+    geom.vertices.push(v2);
+    geom.vertices.push(v3);
+
+    geom.faces.push(new THREE.Face3(0, 1, 2));
+    geom.computeFaceNormals();
+    var mesh = new THREE.Mesh(geom, new THREE.MeshNormalMaterial());
+    scene.add(mesh);
+
     scene.add(cube);
     renderer.render(scene, camera);
 
