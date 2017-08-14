201afdd9a5036c3f1c8abc7f16e0b94bfdebb21c
daveying <daveying@163.com>
2017-08-14 22:54:55 +0800

    add rotate/pan/scale control to curve-triangle viewer

--- js/display.js
+++ js/display.js
@@ -4,8 +4,8 @@ var displayModule = (function () {
         canvas: viewer
     });
     renderer.setClearColor(0xeeeeee);
+    renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(viewer.getBoundingClientRect().width, viewer.getBoundingClientRect().height);
-    console.log(viewer.getBoundingClientRect());
 
     var scene = new THREE.Scene();
 
@@ -13,12 +13,22 @@ var displayModule = (function () {
     camera.position.set(2, 0, 15);
     scene.add(camera);
 
+    var controls = new THREE.TrackballControls(camera, viewer);
+    controls.rotateSpeed = 4.0;
+    controls.zoomSpeed = 4.2;
+    controls.panSpeed = 0.8;
+    controls.noZoom = false;
+    controls.noPan = false;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
+    controls.keys = [65, 83, 68];
+    controls.addEventListener('change', render);
+
     var cube = new THREE.Mesh(new THREE.CubeGeometry(1, 2, 3), new THREE.MeshNormalMaterial());
     var geom = new THREE.Geometry();
     var v1 = new THREE.Vector3(0, 0, 0);
     var v2 = new THREE.Vector3(3, 0, 0);
     var v3 = new THREE.Vector3(3, 3, 0);
-    console.log(geom.vertices);
     geom.vertices.push(v1);
     geom.vertices.push(v2);
     geom.vertices.push(v3);
@@ -27,8 +37,30 @@ var displayModule = (function () {
     geom.computeFaceNormals();
     var mesh = new THREE.Mesh(geom, new THREE.MeshNormalMaterial());
     scene.add(mesh);
-
     scene.add(cube);
-    renderer.render(scene, camera);
+
+    window.addEventListener('resize', onWindowResize, false);
+
+    render();
+    animate();
+    function onWindowResize() {
+        camera.aspect = viewer.getBoundingClientRect().width / viewer.getBoundingClientRect().height;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize( viewer.getBoundingClientRect().width, viewer.getBoundingClientRect().height );
+
+        controls.handleResize();
+
+        render();
+    }
+
+    function animate() {
+        requestAnimationFrame( animate );
+        controls.update();
+    }
+
+    function render() {
+        renderer.render( scene, camera );
+    }
 
 })();
