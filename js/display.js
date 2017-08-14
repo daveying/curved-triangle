5adfbb6db6ce26087856dfcfd87253f78024f8f4
daveying <daveying@163.com>
2017-08-14 23:06:35 +0800

    fix bug related to resize(three.js canvas cannot resize according to window resize)

--- js/display.js
+++ js/display.js
@@ -1,26 +1,25 @@
 var displayModule = (function () {
-    var viewer = document.getElementById('viewer');
-    var renderer = new THREE.WebGLRenderer({
-        canvas: viewer
-    });
+    var viewerDiv = document.getElementById('viewer-div');
+    var renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(0xeeeeee);
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(viewer.getBoundingClientRect().width, viewer.getBoundingClientRect().height);
+    renderer.setSize(viewerDiv.getBoundingClientRect().width, viewerDiv.getBoundingClientRect().height);
+    viewerDiv.appendChild(renderer.domElement);
 
     var scene = new THREE.Scene();
 
-    var camera = new THREE.PerspectiveCamera(45, viewer.getBoundingClientRect().width / viewer.getBoundingClientRect().height, 1, 1000);
-    camera.position.set(2, 0, 15);
+    var camera = new THREE.PerspectiveCamera(45, viewerDiv.getBoundingClientRect().width / viewerDiv.getBoundingClientRect().height, 1, 1000);
+    camera.position.set(0, 0, 15);
     scene.add(camera);
 
-    var controls = new THREE.TrackballControls(camera, viewer);
+    var controls = new THREE.TrackballControls(camera, viewerDiv);
     controls.rotateSpeed = 4.0;
     controls.zoomSpeed = 4.2;
     controls.panSpeed = 0.8;
     controls.noZoom = false;
     controls.noPan = false;
     controls.staticMoving = true;
-    controls.dynamicDampingFactor = 0.3;
+    controls.dynamicDampingFactor = 1.3;
     controls.keys = [65, 83, 68];
     controls.addEventListener('change', render);
 
@@ -44,13 +43,10 @@ var displayModule = (function () {
     render();
     animate();
     function onWindowResize() {
-        camera.aspect = viewer.getBoundingClientRect().width / viewer.getBoundingClientRect().height;
+        camera.aspect = viewerDiv.getBoundingClientRect().width / viewerDiv.getBoundingClientRect().height;
         camera.updateProjectionMatrix();
-
-        renderer.setSize( viewer.getBoundingClientRect().width, viewer.getBoundingClientRect().height );
-
+        renderer.setSize( viewerDiv.getBoundingClientRect().width, viewerDiv.getBoundingClientRect().height );
         controls.handleResize();
-
         render();
     }
 
